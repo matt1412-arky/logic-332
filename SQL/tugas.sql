83d584337ba084426074446820af73c1f80be6cd
Eka Satria Maheswara <ekamaheswara22@gmail.com>
2023-10-31 08:17:11 +0700

    soal tugas

---
+++ SQL/tugas.sql
@@ -0,0 +1,20 @@
+-- 02. Buatlah query untuk mengubah column nama_dosen dengan type data varchar dengan panjang 200 pada table
+
+-- 03. Buatlah query untuk menampilkan data berikut:
+	-- | kode_mahasiswa | nama_mahasiswa | nama_jurusan 		| agama |
+	-- | M001			| Budi Gunawan	 | Teknik Informatika	| Islam |
+
+-- 04. Buatlah query untuk menampilkan data mahasiswa yang mengambil jurusan dengan status jurusan = non aktif
+
+-- 05. Buatlah query untuk menampilkan data mahasiswa dengan nilai diatas 80 untuk ujian dengan status ujian = aktif
+
+-- 06. Buatlah query untuk menampilkan data jurusan yang mengandung kata 'sistem'
+
+-- 07. Buatlah query untuk menampilkan mahasiswa yang mengambil ujian lebih dari 1
+
+-- 08. Buatlah query untuk menampilkan data seperti berikut:
+	-- | kode_mahasiswa	| nama_mahasiswa	| nama_jurusan			| Agama	| nama_dosen					| status_jurusan	| Deskripsi	|
+	-- | M001			| Budi Gunawan		| Teknik Informatika	| Islam	| Prof. Dr. Retno Wahyuningsih	| aktif				| Honorer	|
+
+-- 09. Buatlah query untuk create view dengan menggunakan data pada no. 8, beserta query untuk mengeksekusi view tersebut
+-- 10. Buatlah query untuk menampilkan data mahasiswa beserta nilainya (mahasiswa yang tidak punya nilai juga ditampilkan)
